576d1ad8bc3beada26acc08824b2bbbfc976da70
julien prugne <julien.prugne@zenika.com>
2019-01-29 12:28:46 +0100

    crawl events form offset 0

--- events.js
+++ events.js
@@ -3,7 +3,7 @@ const request = require("./request")
 module.exports = async (page=1) => {
   const pageSize = 50;
   const events = await request(
-    `events?limit=${pageSize}&offset=${page*pageSize-1}`
+    `events?limit=${pageSize}&offset=${page === 0 ? 0 : page*pageSize-1}`
   )
   return events;
 }
