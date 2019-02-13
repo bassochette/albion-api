6eab6f1d548471062dbeccc989b1850ad819583d
julien prugne <julien.prugne@gmail.com>
2019-02-13 16:36:25 +0100

    change default top limit

--- topGatheringGuild.js
+++ topGatheringGuild.js
@@ -1,5 +1,5 @@
 // https://gameinfo.albiononline.com/api/gameinfo/players/statistics?range=week&limit=11&offset=0&type=Gathering&subtype=All&region=Total&guildId=hX4eUgsXSo-YBybi6jcxDw
 const request = require("./request")
-module.exports = async (guildId, range="week", offset=0, limit=20) => request(
+module.exports = async (guildId, range="week", offset=0, limit=10) => request(
     `players/statistics?range=${range}&limit=${limit}&offset=${offset}&type=Gathering&subtype=All&region=Total&guildId=${guildId}`
 )
--- topPvpGuild.js
+++ topPvpGuild.js
@@ -1,5 +1,5 @@
 // /events/playerfame?limit=11&offset=0&range=week&guildId=hX4eUgsXSo-YBybi6jcxDw
 const request = require("./request")
-module.exports = async (guildId, range="week", offset=0, limit=20) => request(
+module.exports = async (guildId, range="week", offset=0, limit=10) => request(
     `events/playerfame?limit=${limit}&offset=0&range=${range}&guildId=${guildId}`
 )
