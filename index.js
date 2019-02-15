1e21f7a9b7e5f57a06304c4aebe466a8f3465569
julien prugne <julien.prugne@gmail.com>
2019-02-15 11:47:16 +0100

    add battles to index

--- index.js
+++ index.js
@@ -1,15 +1,16 @@
 module.exports = {
- deaths : require("./deaths"),
- events : require("./events"),
- gvg : require("./gvg"),
- pastGvg : require("./pastGvgs"),
- player : require("./player"),
- search : require("./search"),
- solokills : require("./soloKills"),
- topKills : require("./topKills"),
- upcomingGvgs : require("./upcomingGvgs"),
+    deaths : require("./deaths"),
+    events : require("./events"),
+    gvg : require("./gvg"),
+    pastGvg : require("./pastGvgs"),
+    player : require("./player"),
+    search : require("./search"),
+    solokills : require("./soloKills"),
+    topKills : require("./topKills"),
+    upcomingGvgs : require("./upcomingGvgs"),
     topPvpGuild: require('./topPvpGuild'),
     topPveGuild: require('./topPveGuild'),
     topCraftingGuild: require('./topCraftingGuild'),
-    topGatheringGuild: require('./topGatheringGuild')
+    topGatheringGuild: require('./topGatheringGuild'),
+    battles: require('./battles')
 }
