ce4cec69f570dc3ee57b293b50860466f946b569
Julien Prugne <julien@webeleon.dev>
2021-03-14 18:13:10 +0100

    fix: remove built folder

--- lib/Requester/Requester.d.ts
+++
@@ -1,6 +0,0 @@
-export declare class Requester {
-    private readonly apiURL;
-    constructor(apiURL?: string);
-    get<ReturnType, QueryParams = any>(path: any, query: QueryParams): Promise<ReturnType>;
-    request<ReturnType>(url: string, method: string): Promise<ReturnType>;
-}
--- lib/main.d.ts
+++
@@ -1,5 +0,0 @@
-import { Events } from './Events/Events';
-export declare class AlbionKillboard {
-    readonly events: Events;
-    constructor(events?: Events);
-}
